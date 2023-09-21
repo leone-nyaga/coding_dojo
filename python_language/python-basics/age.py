ed200d69a446b9c89748998a8186b09236d2c341
leone-nyaga <leonenyaga@gmail.com>
2023-09-21 21:39:36 +0300

    .py

---
+++ python_language/python-basics/age.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+# Ask the user to input their age
+age = int(input("Please enter your age: "))
+
+# Check if the age is greater than or equal to 18
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
---
+++ python_language/python-basics/name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Asks user to input their name
+"""
+
+# Asks user to input their name
+
+name = input("Please enter your name: ")
+
+
+# Prints a personallized message
+print("Hello ", name)
