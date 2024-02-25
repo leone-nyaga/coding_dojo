78fc3171ba67ab0a99287413f975656dee959852
leone-nyaga <leonenyaga@gmail.com>
2024-02-25 14:13:16 +0300

    JS

---
+++ javascript_language/js_fundamentals/string_length.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let myString = "Hello, World!";
+let lengthOfString = myString.length;
+
+console.log("The length of the string is: " + lengthOfString);
+
+
+let myStr = "Hello Love, the cavalry is here!";
+let lenghtOfstr = myStr.length;
+
+console.log("Length of string is, " + lenghtOfstr);
