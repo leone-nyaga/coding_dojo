89f36b144b38574ad1488d867e0f1cc2cc44bda7
leone-nyaga <leonenyaga@gmail.com>
2024-03-06 14:09:29 +0300

    push all

---
+++ C-language/C-Functions_and_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - the function
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar (i);
+		i++;
+	}
+	putchar('\n');
+
+}
