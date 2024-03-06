2574a0646586e4c64d77f7d893399e3b709a49d4
leone-nyaga <leonenyaga@gmail.com>
2024-03-06 15:02:10 +0300

    fn

---
+++ python_language/python-basics/functions/employee.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def show_employee(name, salary=9000):
+    print(f"Your Name {name}, and your salary {salary}.")
+
+show_employee("Ken", "15000")
+show_employee("Mary", "10000")
+show_employee("Mike")
