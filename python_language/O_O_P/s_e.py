12919e3ab5bf3c000e044fd182d77f2f985866fb
leone-nyaga <leonenyaga@gmail.com>
2024-04-14 02:53:12 +0300

    OOP

---
+++ python_language/O_O_P/s_e.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class SoftwareEngineer:
+
+    def __init__(self, name, age, level, salary):
+        #instance attributes
+        self.name = name
+        self.age = age
+        self.level = level
+        self.salary = salary
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, Level: {self.level}, Salary: {self.salary}"
+
+#instance
+se1 = SoftwareEngineer("Mike", 22, "Junior", 75000)
+print(se1)
