818932fec1e31890446b69db825ab70963a92ff6
leone-nyaga <leonenyaga@gmail.com>
2024-05-01 22:58:17 +0300

    OOP

--- python_language/O_O_P/bankAccount_project.py
+++ python_language/O_O_P/bankAccount_project.py
@@ -4,3 +4,8 @@ from bank_accounts import *
 
 Dave = BankAccount(1000, "Dave")
 Sara = BankAccount(2000, "Sara")
+
+Dave.getBalance()
+Sara.getBalance()
+
+Sara.deposit(1000)
--- python_language/O_O_P/bank_accounts.py
+++ python_language/O_O_P/bank_accounts.py
@@ -4,4 +4,13 @@ class BankAccount:
     def __init__(self, initialAmount, accName):
         self.balance = initialAmount
         self.name = accName
-        print(f"\nAccount '{self.name}' created.\nBalance = $'{self.balance:.2f}'")
+        print(f"\nAccount '{self.name}' created.\nBalance = ${self.balance:.2f}")
+
+    def getBalance(self):
+        print(f"\nAccount '{self.name}'.\nBalance = ${self.balance:.2f}")
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print("\nDeposit complete!")
+        self.getBalance()
+
