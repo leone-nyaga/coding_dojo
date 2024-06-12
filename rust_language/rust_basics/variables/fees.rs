6fa34d92c10601d801b288eec52032a5feb6b627
leone-nyaga <leonenyaga@gmail.com>
2024-06-12 13:49:59 +0300

    Rust Variables

---
+++ rust_language/rust_basics/variables/fees.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let fees = 25_000;//imutable
+    let salary = 30_000;//imutable
+
+    println!("Fees is {} and my salary is {}",fees,salary);
+}
---
+++ rust_language/rust_basics/variables/mut.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut y = 20;//mutable value
+    println!("The current value is {}",y);
+
+    let y = 30;//new value
+    println!("The new value is {}",y);
+}
+
