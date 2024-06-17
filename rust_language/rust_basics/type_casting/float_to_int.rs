ea3f22362bb9be761aa9c8486aaaca494de0341d
leone-nyaga <leonenyaga@gmail.com>
2024-06-17 11:42:00 +0300

    type casting

---
+++ rust_language/rust_basics/type_casting/float_to_int.rs
@@ -0,0 +1,11 @@
+/*
+ * converting floating-point to an integer
+ */
+
+fn main () {
+let decimal: f64 = 54.321;
+
+let integer = decimal as u16;
+
+println!("The value of integer is: {}", integer);
+}
