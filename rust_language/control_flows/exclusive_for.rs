f48623d5c99f5d39200ec8c71221c0ff0aa54daf
leone-nyaga <leonenyaga@gmail.com>
2024-06-19 12:52:34 +0300

    control flows

---
+++ rust_language/control_flows/exclusive_for.rs
@@ -0,0 +1,9 @@
+/*
+ * iterating over an exclusive range
+ * meaning the last number will not be included
+ */
+fn main() {
+    for number in 1..5 {
+        println!("The number is: {}", number);
+    }
+}
---
+++ rust_language/control_flows/inclusive_for.rs
@@ -0,0 +1,9 @@
+/*
+ * inclusive range
+ * the last number is included
+ */
+fn main() { 
+    for number in 1..=5 {
+        println!("The number is: {}", number);
+    }
+}
