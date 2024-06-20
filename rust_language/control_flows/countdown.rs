209d1da5463e4e7e12e4f4277f7874a363b297ea
leone-nyaga <leonenyaga@gmail.com>
2024-06-20 14:51:53 +0300

    control flows

---
+++ rust_language/control_flows/countdown.rs
@@ -0,0 +1,13 @@
+/*
+ * counting down using while looping
+ */
+
+fn main() {
+    let mut counter = 5;
+
+    while counter > 0 {
+        println!("{}", counter);
+        counter-=1;
+    }
+    println!("HAPPY NEW YEAR!!!");
+}
