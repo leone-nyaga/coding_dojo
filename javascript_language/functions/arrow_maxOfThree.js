dd86de0cd6aadab33c45d9b45a240b93560ae7da
leone-nyaga <leonenyaga@gmail.com>
2024-07-20 16:35:54 +0300

    functions and arrow functions

---
+++ javascript_language/functions/arrow_maxOfThree.js
@@ -0,0 +1,6 @@
+const maxOfThree = (a, b , c) => Math.max(a, b ,c );
+
+console.log(maxOfThree(12, 239,632));
+console.log(maxOfThree(1, 399,62));
+console.log(maxOfThree(10, 439,762));
+console.log(maxOfThree(101, 239,62));
---
+++ javascript_language/functions/maxOfThree.js
@@ -0,0 +1,8 @@
+function maxOfThree(a, b, c) {
+	return Math.max(a, b, c);
+}
+
+console.log(maxOfThree(60, 39,62));
+console.log(maxOfThree(40, 379,62));
+console.log(maxOfThree(10, 639,62));
+console.log(maxOfThree(109, 43, 16));
