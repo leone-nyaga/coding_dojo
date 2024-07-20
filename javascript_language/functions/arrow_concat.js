096debd1db713425c4491dc19c60271b2fdcd4ef
leone-nyaga <leonenyaga@gmail.com>
2024-07-20 16:25:49 +0300

    functions and arrow functions

---
+++ javascript_language/functions/arrow_concat.js
@@ -0,0 +1,4 @@
+const concat = (string1, string2) => string1 + ' ' + string2;
+
+console.log(concat("Hello", "World"));
+console.log(concat("Hello", "Javascript"));
---
+++ javascript_language/functions/concat.js
@@ -0,0 +1,6 @@
+function concat(string1, string2) {
+	return string1 + ' ' + string2;
+}
+
+console.log(concat("Hello", "World"));
+console.log(concat("Hello", "World")); 
