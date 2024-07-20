18b451c06016982f61cb31ef6e1d2b5f91905072
leone-nyaga <leonenyaga@gmail.com>
2024-07-20 16:19:37 +0300

    functions and arrow functions

---
+++ javascript_language/functions/arrow_isEven.js
@@ -0,0 +1,5 @@
+const isEven = (num) => num % 2 === 0;
+
+console.log(isEven(24));
+console.log(isEven(21));
+console.log(isEven(19));
---
+++ javascript_language/functions/isEven.js
@@ -0,0 +1,6 @@
+function isEven(num) {
+	return num % 2 === 0;
+}
+
+console.log(isEven(6));
+console.log(isEven(5));
