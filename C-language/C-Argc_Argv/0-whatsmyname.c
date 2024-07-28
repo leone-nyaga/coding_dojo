5701c3ce9b295075b248867860a2a706c39173cc
leone-nyaga <leonenyaga@gmail.com>
2024-07-28 19:36:05 +0300

    command line arguments

---
+++ C-language/C-Argc_Argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name of file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: zero
+ */
+
+int main(int argc, char * argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
