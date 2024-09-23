49489e6e63ad2412c5f9ee7f71d03684b505ac16
leone-nyaga <leonenyaga@gmail.com>
2024-09-24 02:25:38 +0300

    C STRUCTS

---
+++ C-language/C-structs/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct Student - Information about a student
+ * @name: name of student
+ * @age: age of student
+ * @grade: grade of student
+ */
+struct Student
+{
+	char name[50];
+	int age;
+	float grade;
+};
