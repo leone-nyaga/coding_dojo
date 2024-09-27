b15f861ca4efffa1916246934112ce5350c5f8c2
leone-nyaga <leonenyaga@gmail.com>
2024-09-28 02:18:51 +0300

    File i/o

--- C-language/C-file_io/write-file.c
+++ C-language/C-file_io/write-file.c
@@ -4,19 +4,26 @@
  * createAndWriteFile - Creates and writes to a file
  * @filename: name of the file
  * @text: text to be written
- * Return: 0
+ * Return: void
  */
 void createAndWriteFile(const char *filename, const char *text)
 {
+	/* opens file in write mode */
 	FILE *file = fopen(filename, "w");
+
+	/* checks if file is opened successfully */
 	if (file == NULL) 
 	{
 		perror("Error opening file");
 		return;
 	}
 
+	/* write text to file */
 	fprintf(file, "%s", text);
+
+	/* closes a file */
 	fclose(file);
+
 	printf("File '%s' created and written to successfully.\n", filename);
 }
 
