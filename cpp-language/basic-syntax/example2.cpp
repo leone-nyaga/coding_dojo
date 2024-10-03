1346a68246c88e7804ed8a10175429790fee4508
leone-nyaga <leonenyaga@gmail.com>
2024-10-03 22:12:35 +0300

    C++ Basics

---
+++ cpp-language/basic-syntax/example2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	int age = 30;           // Integer variable
+	float height = 5.9f;    // Float variable
+	char initial = 'A';     // Character variable
+	bool isStudent = false; // Boolean variable
+	
+	// Output values
+	std::cout << "Age: " << age << std::endl;
+	std::cout << "Height: " << height << std::endl;
+	std::cout << "Initial: " << initial << std::endl;
+	std::cout << "Is Student: " << isStudent << std::endl;
+	
+	return 0;
+}		
