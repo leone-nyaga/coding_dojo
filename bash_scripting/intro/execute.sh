afcc225f06b468eef005d099b292b9c5b96078fb
leone-nyaga <leonenyaga@gmail.com>
2024-10-07 16:31:43 +0300

    permissions

---
+++ bash_scripting/intro/execute.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script that adds execute permission to the owner of the file filename
+
+chmod 100 filename
---
+++ bash_scripting/intro/multiple.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#script that adds execute permission to the owner and the group owner, and read permission to other users, to the file filename
+
+chmod ug+x,o+r filename
