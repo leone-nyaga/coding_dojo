4f051a315c95eb5be7b9a6f7d405c67535c4c989
leone-nyaga <leonenyaga@gmail.com>
2024-11-17 13:02:44 +0300

    C DMA

--- C-language/C-pointers_arrays_and_strings/main.h
+++ C-language/C-pointers_arrays_and_strings/main.h
@@ -4,10 +4,13 @@
 #include <stdio.h>
 
 int main(void);
+int _putchar(char c);
 void printArray(int arr[], int size);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
 
 #endif
