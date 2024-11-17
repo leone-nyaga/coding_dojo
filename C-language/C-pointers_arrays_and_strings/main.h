b2cde004cb45850a856bf3bba4b7efa6c43c6bc4
leone-nyaga <leonenyaga@gmail.com>
2024-11-17 15:39:59 +0300

    C pointers

--- C-language/C-pointers_arrays_and_strings/main.h
+++ C-language/C-pointers_arrays_and_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
 
 int main(void);
 int _putchar(char c);
--- C-language/C-pointers_arrays_and_strings/puts_half.c
+++ C-language/C-pointers_arrays_and_strings/puts_half.c
@@ -1,8 +1,32 @@
 #include "main.h"
 
+/**
+ * puts_half - prints the second half of a string, followed by a new line
+ * @str:the string to print
+ */
+
 void puts_half(char *str)
 {
+    int len = strlen(str);  // Get the length of the string
+    int start_index;
 
+    // If the length is even, start from len / 2, else from (len / 2) + 1
+    if (len % 2 == 0)
+    {
+        start_index = len / 2;  // Even length: start from middle
+    }
+    else
+    {
+        start_index = (len / 2) + 1;  // Odd length: start just after the middle
+    }
+
+    // Print the second half of the string starting from start_index
+    for (int i = start_index; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);  // Print character
+    }
+    _putchar('\n');  // Print a new line after the second half
+}
 
 
 /**
