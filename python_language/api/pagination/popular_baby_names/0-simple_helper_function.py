1342418ee553ee29c4d6b4f934acc09171e6121d
leone-nyaga <leonenyaga@gmail.com>
2024-11-21 15:07:23 +0300

    Popular baby names

--- python_language/api/pagination/popular_baby_names/0-simple_helper_function.py
+++ python_language/api/pagination/popular_baby_names/0-simple_helper_function.py
@@ -9,4 +9,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     return a tuple of size two containing a start index and an end index
     """
-    return ((page - 1) * page_size, page * page_size)
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
