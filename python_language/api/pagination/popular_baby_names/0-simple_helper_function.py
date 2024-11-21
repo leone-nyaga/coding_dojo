6aed90424bb1cd683280d7e2d0531de9b35f7c8c
leone-nyaga <leonenyaga@gmail.com>
2024-11-21 14:58:39 +0300

    Popular baby names

---
+++ python_language/api/pagination/popular_baby_names/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes two integer arguments page and page_size.
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start index and an end index
+    """
+    return ((page - 1) * page_size, page * page_size)
