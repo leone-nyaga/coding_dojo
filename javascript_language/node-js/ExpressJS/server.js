054a0d4c629f41b17bf522bd7b799ba71f664d13
leone-nyaga <leonenyaga@gmail.com>
2024-12-18 17:42:30 +0300

    Express js

--- javascript_language/node-js/ExpressJS/server.js
+++ javascript_language/node-js/ExpressJS/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/hello', (req, res) => {
   res.send('HELLO WORLD');
 });
 
-app.listen(5000);
+app.get('/index1', (req, res) => {
+  const name = 'Mike';
+  res.render('index', {name});
+});
+
+app.listen((5000), () => {
+  console.log('app listening on port 5000');
+});
