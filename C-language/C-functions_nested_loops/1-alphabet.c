d68b27e950deae10a851bb4a46b4add3a949451b
leone-nyaga <leonenyaga@gmail.com>
2025-07-15 15:18:17 +0300

    Functions

--- C-language/C-functions_nested_loops/1-alphabet.c
+++ C-language/C-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,6 @@
 /**
  * print_alphabet -prints the alphabet, in lowercase
  *
- * Return: 0
  */
 
 void print_alphabet(void)
--- C-language/C-functions_nested_loops/2-print_alphabet_x10.c
+++ C-language/C-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,6 @@
 /**
  * void print_alphabet_x10 - function that prints 10 times the alphabet,
  * in lowercase, followed by a new line.
- * Return: 0
  */
 
 void print_alphabet_x10(void)
