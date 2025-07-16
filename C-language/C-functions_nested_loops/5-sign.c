4ef49c9deb3b0a9649cde18e6ea45cc9120733e4
leone-nyaga <leonenyaga@gmail.com>
2025-07-16 23:41:45 +0300

    FUNCTIONS

--- C-language/C-functions_nested_loops/5-sign.c
+++ C-language/C-functions_nested_loops/5-sign.c
@@ -1,30 +1,72 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_sign - sign of a number
- * @n: character
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * print_sign - Prints the sign of a number
+ * @n: The number to check
+ *
+ * Return: 1 if n > 0, 0 if n == 0, -1 if n < 0
  */
-
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-
-	else (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
 }
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int r;
+
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    if (r < 0)
+    {
+        _putchar('-');
+        _putchar(-r + '0');
+    }
+    else
+    {
+        _putchar(r + '0');
+    }
+    _putchar('\n');
+
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar('-');
+    _putchar('1');
+    _putchar('\n');
+
+    return (0);
+}
+
