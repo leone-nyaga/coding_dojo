e628f689772935086e3c5874e3b171ebc5f1b402
leone-nyaga <125727613+leone-nyaga@users.noreply.github.com>
2025-11-03 23:14:48 +0300

    Delete javascript_language/js_promises directory

--- javascript_language/js_promises/0-addNums.js
+++
@@ -1,9 +0,0 @@
-export default function addNumbers(a, b) {
-    return new Promise((resolve, reject) => {
-        if (typeof a === 'number' && typeof b === 'number') {
-            resolve(a + b); // If both values are numbers, return the sum
-        } else {
-            reject('Both arguments must be numbers'); // If any argument is not a number, reject
-        }
-    });
-}
--- javascript_language/js_promises/0-main.js
+++
@@ -1,9 +0,0 @@
-import addNumbers from './0-addNums';
-
-addNumbers(5, 10)
-    .then(result => {
-        console.log('The sum is:', result); // Will log "The sum is: 15"
-    })
-    .catch(error => {
-        console.error('Error:', error); // Will log an error if inputs are invalid
-    });
