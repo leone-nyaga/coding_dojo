b435ad715dc8575a2bedc8d1c935b610360272c1
leone-nyaga <leonenyaga@gmail.com>
2026-03-17 12:51:04 +0300

    Print name of file

---
+++ C-language/C-argc_argv/mynameis.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
