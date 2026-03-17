a9771db2e5c515cf4cee7429b3b83172eb912f5f
leone-nyaga <leonenyaga@gmail.com>
2026-03-17 17:41:01 +0300

    Command line arguments

--- C-language/C-argc_argv/sum.c
+++ C-language/C-argc_argv/sum.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int sum = 0, i;
+
 	for (i = 1; i < argc; i++)
 	{
 		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
+
 	return (0);
 }
