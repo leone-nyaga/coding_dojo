1a4f0017d50cea301be3e4333ae03aa25bc7b7a5
leone-nyaga <leonenyaga@gmail.com>
2026-03-17 12:21:51 +0300

    What are the numbers of argument

---
+++ C-language/C-argc_argv/arg_count.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @argc: Argument count
+ * @argv: Array of Arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("Arg count %d\n", argc);
+	return (0);
+}
