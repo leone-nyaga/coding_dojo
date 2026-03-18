6951b125afb273195dcff5ee92c00f2b45a1939c
leone-nyaga <leonenyaga@gmail.com>
2026-03-18 20:49:37 +0300

    C Preprocessor

---
+++ C-language/C-preprocessing/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_h
+
+#include <stdio.h>
+
+#define SIZE 1024
+
+
+#endif
---
+++ C-language/C-preprocessing/size-token.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int s;
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
