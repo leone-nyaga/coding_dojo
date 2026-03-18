0811cc44ed16b6469a6301e54042be1d1abb1f50
leone-nyaga <leonenyaga@gmail.com>
2026-03-18 21:15:53 +0300

    C Preprocessor

--- C-language/C-preprocessing/main.h
+++ C-language/C-preprocessing/main.h
@@ -5,5 +5,8 @@
 
 #define SIZE 1024
 #define PI 3.14159265359
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+#define SUM(x, y) ((x) + (y))
+#define MESSAGE() printf("HELLO WORLD!\n");
 
 #endif
---
+++ C-language/C-preprocessing/message.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	MESSAGE();
+	return (0);
+}
