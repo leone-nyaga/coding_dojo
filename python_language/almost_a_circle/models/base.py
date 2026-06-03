4519c19852eaf808312d161a066305b141079716
leone-nyaga <leonenyaga@gmail.com>
2026-06-03 13:54:15 +0300

    Added docstrings

--- python_language/almost_a_circle/models/base.py
+++ python_language/almost_a_circle/models/base.py
@@ -2,9 +2,29 @@
 
 
 class Base:
+    """
+    Base class for managing object IDs.
+
+    If an ID is provided during instantiation, it is used directly.
+    Otherwise, an auto-incremented ID is assigned.
+
+    Attributes:
+        __nb_objects (int): Class-level counter tracking the
+        number of objects created.
+        id (int): Unique identifier for each instance.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initialize a new Base instance.
+
+         Args:
+            id: The ID to assign to the instance.
+            If None, a unique ID is automatically generated.
+        """
+
         if id is not None:
             self.id = id
         else:
--- python_language/almost_a_circle/models/rectangle.py
+++ python_language/almost_a_circle/models/rectangle.py
@@ -4,7 +4,21 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    Rectangle class that inherits from the Base class.
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a new Rectangle instance.
+
+        Args:
+            width: Width of rectangle.
+            height: Height of rectagle.
+            x: Horizontal position.
+            y: Vertical position.
+            id: identifier passed to Base.
+        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -13,32 +27,56 @@ class Rectangle(Base):
 
         @property
         def width(self):
+            """
+            Width of the rectangle.
+            """
             return self.__width
 
         @width.setter
         def width(self, value):
+            """
+            Setter to width.
+            """
             self.__width = value
 
         @property
         def height(self):
+            """
+            Getter to height.
+            """
             return self.__height
 
         @height.setter
         def height(self):
+            """
+            Setter to height.
+            """
             self.__height = value
 
         @property
         def x(self):
+            """
+            Getter to x
+            """
             return self.__x
 
         @x.setter
         def x(self, value):
+            """
+            Setter to x.
+            """
             self.__x = value
 
         @property
         def y(self):
+            """
+            Getter to y.
+            """
             return self.__y
 
         @y.setter
         def y(self, value):
+            """
+            Setter to y.
+            """
             self.__y = value
