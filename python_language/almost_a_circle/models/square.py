06a4dbd2dd0806fd6d73662d7390c99175cf8bc4
leone-nyaga <leonenyaga@gmail.com>
2026-07-01 12:34:28 +0300

    updates args and kwargs for square class

--- python_language/almost_a_circle/models/square.py
+++ python_language/almost_a_circle/models/square.py
@@ -41,3 +41,27 @@ class Square(Rectangle):
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
+    def update(self, *args, **kwargs):
+        """
+        Update the Square attributes using *args in order or **kwargs by key.
+        """
+        if args and args != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
