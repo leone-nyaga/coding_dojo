30e7e09788ecacec445356e9c17c499ac32c8b3e
leone-nyaga <leonenyaga@gmail.com>
2026-07-01 21:40:02 +0300

    static and class methods

--- python_language/almost_a_circle/models/base.py
+++ python_language/almost_a_circle/models/base.py
@@ -42,3 +42,19 @@ class Base:
             return "[]"
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write JSON string representation of list_objs to a file."""
+
+        filename = cls.__name__ + ".json"
+
+        if list_objs is None:
+            list_dictionaries = []
+        else:
+            list_dictionaries = [obj.to_dictionary() for obj in list_objs]
+
+        json_string = cls.to_json_string(list_dictionaries)
+
+        with open(filename, "w") as f:
+            f.write(json_string)
